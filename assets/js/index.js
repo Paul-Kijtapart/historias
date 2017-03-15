132807fdd22f8a1b1009959aecf199895f57158f
Paul-Kijtapart <scc385@gmail.com>
2017-03-14 19:12:21 -0700

    add carousel to the header in farmer page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,28 @@
-// Toggle NavBar
-$(document).ready(function() {
-	$('.right.menu.open').on("click", function(e) {
-		e.preventDefault();
-		$('.ui.vertical.menu').toggle();
+jQuery(document).ready(function($) {
+
+	// Toggle NavBar
+	$(document).ready(function() {
+		$('.right.menu.open').on("click", function(e) {
+			e.preventDefault();
+			$('.ui.vertical.menu').toggle();
+		});
+
+		$('.ui.dropdown').dropdown();
 	});
 
-	$('.ui.dropdown').dropdown();
-});
+	$('button#back_to_top_btn').on("click", function(e) {
+		e.preventDefault();
+		console.log(e.target);
+		$("html, body").animate({
+			scrollTop: 0
+		}, "slow");
+	});
 
-$('button#back_to_top_btn').on("click", function(e) {
-	e.preventDefault();
-	console.log(e.target);
-	$("html, body").animate({
-		scrollTop: 0
-	}, "slow");
+	$(".farmerImagesSlide").slick({
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true
+	});
 });
